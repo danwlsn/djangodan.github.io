0d0fca4660985af4ef4474eca34256997071ec87
Danny Wilson <dannywilson32@gmail.com>
2014-04-25 15:30:36 +0100

    Caching, minifying, and all the response time optimisers

--- js/main.js
+++ js/main.js
@@ -4,22 +4,4 @@ $( document ).ready(function() {
 	}, function(){
 		$('.hyde').css('display', 'none');
 	})
-
-	// JSON
-	// Instagram
-	// https://api.instagram.com/v1/users/305466902/media/recent?count=3&access_token=305466902.5b9e1e6.604c65a76da44a07945ea4429450c2a1
-
-	// Instagram
-	// $.ajax({
-	// 	type: "GET",
-	// 	dataType: "jsonp",
-	// 	cache: false,
-	// 	url: "https://api.instagram.com/v1/users/305466902/media/recent?count=4&access_token=305466902.5b9e1e6.604c65a76da44a07945ea4429450c2a1",
-	// 	success: function(data) {
-	// 			for (var i = 0; i < 15; i++) {
-	// 				$(".insta--pics").append("<li><a href="+ data.data[i].link +"><img src=" + data.data[i].images.thumbnail.url+ "/></a></li>");
-	// 			}
-
-	// 	}
-	// });
 });
